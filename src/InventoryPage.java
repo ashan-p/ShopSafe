ae0c978afe1c148ab6896de31ecf66caaa22dd88
ashan-p <ashan_p@outlook.com>
2020-11-28 04:39:25 -0500

    new item images and getting started on item popup

--- src/InventoryPage.java
+++ src/InventoryPage.java
@@ -1,15 +1,21 @@
 import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
+import javax.swing.Box;
 
 import java.awt.GridLayout;
 import java.awt.image.BufferedImage;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 
 public class InventoryPage extends JPanel {
 
@@ -20,10 +26,12 @@ public class InventoryPage extends JPanel {
     }
     
     public InventoryPage(User user) {
+        Window window = Window.getInstance();
         Inventory inventory = Inventory.getInstance();
 
         setLayout(new GridLayout(0, 5));
-        ImageIcon image = scaleImageIcon(new ImageIcon("images/table.png"), 64, 64);
+        int size = 85;
+        ImageIcon image = scaleImageIcon(new ImageIcon("images/television.png"), size, size);
         for (int i = 1; i <= 16; i++) {
             JLabel label = new JLabel();
             label.setIcon(image);
@@ -31,6 +39,31 @@ public class InventoryPage extends JPanel {
 
             JButton button = new JButton();
             button.add(label);
+            button.addActionListener(e -> {
+                JDialog dialog = new JDialog(window, "Television");
+                dialog.setLayout(new GridBagLayout());
+
+                JPanel content = new JPanel();
+                content.setLayout(new BoxLayout(content, BoxLayout.Y_AXIS));
+                
+                JLabel picture = new JLabel();
+                picture.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+                picture.setIcon(image);
+
+                JLabel title = new JLabel("Television");
+                title.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+
+                content.add(picture);
+                content.add(Box.createVerticalStrut(30));
+                content.add(title);
+
+                dialog.add(content, new GridBagConstraints());
+    
+                // setsize of dialog 
+                dialog.setSize(350, 300); 
+                dialog.setLocationRelativeTo(window);
+                dialog.setVisible(true);
+            });
 
             add(button);
         }
